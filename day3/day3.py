e8d674870fa17b7439905927fca2eb3da633c03d
Onur Gümüş <onurgumus.96@gmail.com>
2023-01-30 15:29:16 -0500

    Day 3 example

--- day3/day3.py
+++ day3/day3.py
@@ -1,1 +1,19 @@
-print("Hello world")
+todos = []
+
+while True:
+    user_action = input('Type show or add: ')
+
+    match user_action.strip():
+        case 'add':
+            todo = input('Enter a todo: ')
+            todos.append(todo.strip())
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'exit':
+            break
+        case x:
+            print('You entered an unknown command.')
+
+print('Bye!')
