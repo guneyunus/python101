4600492374a19b7f647944d68f66c274b1a693ca
Yunus Guney~ <guney.yunus.dev@outlook.com>
2023-01-30 17:47:35 -0500

    netcat v1

--- black-hat-python/yunus/bhp.py
+++ black-hat-python/yunus/bhp.py
@@ -1,1 +1,17 @@
-print()
+import socket
+
+target_host = "0.0.0.0"
+target_port = 9998
+
+#create a socket object
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#connet the client 
+client.connect((target_host,target_port))
+
+client.send(b"GET / HTTPS/1.1\r\nHost: google.com\r\n\r\n")
+
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
