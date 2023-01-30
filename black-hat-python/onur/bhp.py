b6771c4cdf9f523ad8ff8364dea1c34da80fa033
Onur Gümüş <onurgumus.96@gmail.com>
2023-01-30 17:48:19 -0500

    NetCat file created

--- black-hat-python/onur/bhp.py
+++ black-hat-python/onur/bhp.py
@@ -1,1 +1,19 @@
-print()
+import socket
+
+target_host = '0.0.0.0'
+target_port = 9998
+
+# create socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host, target_port))
+
+# send some data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# receieve some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
---
+++ black-hat-python/onur/udp-client.py
@@ -0,0 +1,16 @@
+import socket
+
+target_host = 'www.google.com'
+target_port = 80
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send some data
+client.sendto(b"AAABBBCCC", (target_host, target_port))
+
+# receive some data
+data, addr = client.recvfrom(4096)
+
+print(data.decode())
+client.close()
